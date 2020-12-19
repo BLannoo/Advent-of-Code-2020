30eebac704d1c52b3974f5a9ea6ec67b4b243107
Bruno Lannoo <Bruno.Lannoo@nike.com>
2020-12-19 13:00:17 +0100

    Solution Day19 Gold

--- day19/day19.py
+++ day19/day19.py
@@ -3,31 +3,99 @@ from typing import Callable, Dict
 
 
 def test_silver_example():
-    rules, messages = parse("example.txt")
-    assert "a" == create_assembler(rules)(4)
-    assert "b" == create_assembler(rules)(5)
-    assert "(ab|ba)" == create_assembler(rules)(3)
-    assert "(aa|bb)" == create_assembler(rules)(2)
-    assert "((aa|bb)(ab|ba)|(ab|ba)(aa|bb))" == create_assembler(rules)(1)
-    assert "a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b" == create_assembler(rules)(0)
-    assert 2 == count_matches(rules, messages)
+    rules, messages = parse("example_silver.txt")
+    assembler = create_silver_assembler(rules)
+    assert "a" == assembler(4)
+    assert "b" == assembler(5)
+    assert "(ab|ba)" == assembler(3)
+    assert "(aa|bb)" == assembler(2)
+    assert "((aa|bb)(ab|ba)|(ab|ba)(aa|bb))" == assembler(1)
+    assert "a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b" == assembler(0)
+    assert 2 == count_matches(silver_matcher_factory(assembler), messages)
 
 
 def test_silver():
     rules, messages = parse("input.txt")
-    assert 265 == count_matches(rules, messages)
+    assembler = create_silver_assembler(rules)
+    assert 265 == count_matches(silver_matcher_factory(assembler), messages)
 
 
-def count_matches(rules, messages):
+def test_gold_example_with_silver_assembler():
+    rules, messages = parse("example_gold.txt")
+    assembler = create_silver_assembler(rules)
+    assert 3 == count_matches(silver_matcher_factory(assembler), messages)
+
+
+def test_gold_example_with_gold_assembler():
+    rules, messages = parse("example_gold.txt")
+    assembler = create_silver_assembler(rules)
+    assert "((b(a(bb|ab)|b(a|b)(a|b))|a(bbb|a(bb|a(a|b))))b|(((aa|ab)a|bbb)b|((a|b)a|bb)aa)a)" == assembler(42)
+    assert "(b(b(aba|baa)|a(b(ab|(a|b)a)|a(ba|ab)))|a(b((ab|(a|b)a)b|((a|b)a|bb)a)|a(bab|(ba|bb)a)))" == assembler(31)
+
+    assert not gold_matcher_factory(assembler)("abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa")
+    assert gold_matcher_factory(assembler)("bbabbbbaabaabba")
+    assert gold_matcher_factory(assembler)("babbbbaabbbbbabbbbbbaabaaabaaa")
+    assert gold_matcher_factory(assembler)("aaabbbbbbaaaabaababaabababbabaaabbababababaaa")
+    assert gold_matcher_factory(assembler)("bbbbbbbaaaabbbbaaabbabaaa")
+    assert gold_matcher_factory(assembler)("bbbababbbbaaaaaaaabbababaaababaabab")
+    assert gold_matcher_factory(assembler)("ababaaaaaabaaab")
+    assert gold_matcher_factory(assembler)("ababaaaaabbbaba")
+    assert gold_matcher_factory(assembler)("baabbaaaabbaaaababbaababb")
+    assert gold_matcher_factory(assembler)("abbbbabbbbaaaababbbbbbaaaababb")
+    assert gold_matcher_factory(assembler)("aaaaabbaabaaaaababaa")
+    assert gold_matcher_factory(assembler)("aaaabbaabbaaaaaaabbbabbbaaabbaabaaa")
+    assert not gold_matcher_factory(assembler)("aaaabbaaaabbaaa")
+    assert not gold_matcher_factory(assembler)("babaaabbbaaabaababbaabababaaab")
+    assert gold_matcher_factory(assembler)("aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba")
+
+    assert 12 == count_matches(gold_matcher_factory(assembler), messages)
+
+
+def test_gold():
+    rules, messages = parse("input.txt")
+    assembler = create_silver_assembler(rules)
+    assert 394 == count_matches(gold_matcher_factory(assembler), messages)
+
+
+def gold_matcher_factory(assembler: Callable[[int], str]) -> Callable[[str], bool]:
+    def matches_gold(message: str) -> bool:
+        match_11 = matches_11(message)
+        match_11_count = 0
+        match_8 = matches_8(message)
+        match_8_count = 0
+        while match_8 or match_11:
+            if match_11:
+                message = match_11.group("rest")
+                match_11_count += 1
+            elif match_8:
+                message = match_8.group("rest")
+                match_8_count += 1
+            match_11 = matches_11(message)
+            match_8 = matches_8(message)
+        return message == "" and match_8_count > 0 and match_11_count > 0
+
+    def matches_8(message):
+        return re.match(assembler(42) + "(?P<rest>.*)$", message)
+
+    def matches_11(message):
+        return re.match(assembler(42) + "(?P<rest>.*)" + assembler(31) + "$", message)
+
+    return matches_gold
+
+
+def silver_matcher_factory(assembler: Callable[[int], str]) -> Callable[[str], bool]:
+    return lambda message: re.match(assembler(0) + "$", message) is not None
+
+
+def count_matches(matcher: Callable[[str], bool], messages):
     return sum([
         1
         for message in messages
-        if re.match(create_assembler(rules)(0) + "$", message)
+        if matcher(message)
     ])
 
 
-def create_assembler(rules: Dict[int, str]):
-
+def create_silver_assembler(rules: Dict[int, str]):
     rules_memory = {}
 
     def assemble(rule_id: int) -> str:
